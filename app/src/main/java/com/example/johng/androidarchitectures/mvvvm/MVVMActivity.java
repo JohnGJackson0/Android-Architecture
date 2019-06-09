5f6b5e744cab490bdc0cb41ee05f1e19344f7afb
John Jackson <johngjackson0@gmail.com>
2019-06-09 08:51:09 -0400

    complete mvvm

--- app/src/main/java/com/example/johng/androidarchitectures/mvvvm/MVVMActivity.java
+++ app/src/main/java/com/example/johng/androidarchitectures/mvvvm/MVVMActivity.java
@@ -1,19 +1,88 @@
 package com.example.johng.androidarchitectures.mvvvm;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.johng.androidarchitectures.R;
+import com.example.johng.androidarchitectures.mvp.CountriesPresenter;
+import com.example.johng.androidarchitectures.mvp.MVPActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MVVMActivity extends AppCompatActivity {
+
+    private List<String> listValues = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView list;
+    private Button retryButton;
+    private ProgressBar progressBar;
+    private CountriesViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvvm);
         setTitle(R.string.titleForMVVMActivity);
+
+        viewModel = ViewModelProviders.of(this).get(CountriesViewModel.class);
+
+        list = findViewById(R.id.list);
+        retryButton = findViewById(R.id.retryButton);
+        progressBar = findViewById(R.id.progress);
+
+        adapter = new ArrayAdapter<>(this, R.layout.row_layout, R.id.listText, listValues);
+
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MVVMActivity.this, "You clicked " + listValues.get(position),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        observeViewModel();
+    }
+
+    private void observeViewModel(){
+        viewModel.getCountries().observe(this, country -> {
+            if(country != null) {
+                listValues.clear();
+                listValues.addAll(country);
+                list.setVisibility(View.VISIBLE);
+                adapter.notifyDataSetChanged();
+            } else {
+                list.setVisibility(View.GONE);
+            }
+        });
+
+        viewModel.getCountryError().observe(this, error -> {
+            progressBar.setVisibility(View.GONE);
+            if(error){
+                Toast.makeText(this, getString(R.string.error_message), Toast.LENGTH_SHORT).show();
+                retryButton.setVisibility(View.VISIBLE);
+            } else {
+                retryButton.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void onRetry(View view) {
+        viewModel.onRefresh();
+        retryButton.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        list.setVisibility(View.GONE);
     }
 
     public static Intent getIntent(Context context) {
